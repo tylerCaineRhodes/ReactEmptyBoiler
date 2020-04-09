e4a9faa701bade0ac90cadacef13cb3631f1c9f3
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-04-09 17:46:11 -0500

    Add simple server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.listen(1234, () => {
+    console.log(`hurray, you're connected!`);
+});
 
 
 
