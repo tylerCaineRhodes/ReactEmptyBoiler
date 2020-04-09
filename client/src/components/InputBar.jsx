849b1c60c860dd58b2eb3dd2c4667a074934db00
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-04-09 13:32:27 -0500

    Refactor to show two approaches to form

--- client/src/components/InputBar.jsx
+++ client/src/components/InputBar.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 //map value and onSubmit
 //onchange can update the state and onsubmit can append that state to the list
 const InputBar = ({onSubmit}) => {
-  var input;
+  let input;
+  
   return (
     // <div>
     //   <input ref={node => (input = node)}></input>
     //   <button onClick={() => onSubmit(input.value)}>Submit</button>
     // </div>
 
-    
     <div>
       <form id='best' onSubmit={(e) => {
         e.preventDefault();
