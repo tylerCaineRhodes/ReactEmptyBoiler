27e151b23ec16116626a6c40822aa092a60d5b0f
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-04-19 11:07:58 -0500

    Add example env setup to protect private variables

--- db/querys.js
+++ db/querys.js
@@ -1,9 +1,12 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'todolist'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.PORT,
 });
 
 connection.connect(err => {
