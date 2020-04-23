02c11f24d60dbdcecdc2c09528c3f350d436791a
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-04-23 18:00:21 -0500

    Use hook for useRef

--- client/src/components/InputBar.jsx
+++ client/src/components/InputBar.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const InputBar = ({onSubmit}) => {
-  let input;
-  
+const InputBar = ({ onSubmit }) => {
+  let input = useRef(null);
+
   return (
     <div>
-      <form id='best' onSubmit={(e) => {
-        e.preventDefault();
-        onSubmit(input.value)
-        document.getElementById('best').reset();
-      }}>
-        <input ref={node => (input = node)}></input>
+      <form
+        id="best"
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          onSubmit(input.current.value);
+          document.getElementById('best').reset();
+        }}
+      >
+        <input ref={input}></input>
         <button type="submit">Submit</button>
       </form>
     </div>
-  )
+  );
 };
 
 export default InputBar;
