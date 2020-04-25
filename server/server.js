67acbb8f36b5be5d25f0519dafb52f9c3c68540d
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-04-24 23:00:28 -0500

    Create mongo solution without mongoose

--- server/server.js
+++ server/server.js
@@ -1,27 +1,25 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 1234;
-// const { postTask, getTasks, deleteTask } = require("../db/querys.js").default;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID
+const dbName = 'todolist';
 
 app.use(express.static(path.join(__dirname, "../client/dist/")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectID
-const dbName = 'todolist';
-
 MongoClient.connect(process.env.MONGO_STRING,{ useUnifiedTopology: true })
   .then(client => {
     console.log('you connected to mongo')
+
     const db = client.db(dbName)
     const tasks = db.collection('tasks')
 
     app.post('/tasks', (req, res) => {
-      console.log(req.body.data)
       tasks.insertOne(req.body.data)
         .then(result => {
           res.send(result)
@@ -31,28 +29,29 @@ MongoClient.connect(process.env.MONGO_STRING,{ useUnifiedTopology: true })
         })
     })
 
+
     app.get('/tasks', (req, res) => {
-      tasks.find({}).toArray((err, results) => {
-        if(err){
-          console.log('err in getting tasks')
-        }
-        console.log(results)
-        res.send(results)
-      })
+      tasks.find({}).toArray()
+        .then(results => {
+          res.send(results)
+        })
+        .catch(err => {
+          console.error('there was an error in getting tasks from server')
+          res.sendStatus(418)
+        })
     })
+
 
     app.delete('/tasks/:id', (req, res) => {
-      tasks.deleteOne({'_id': ObjectId(req.params.id)}, (err, results) => {
-        if(err){
-          console.log('something bad in delete')
-        } else {
-          console.log('deleted the thing?')
-          res.send(results)
-        }
-      })
-
+      tasks.deleteOne({'_id': ObjectId(req.params.id)})
+        .then(response => {
+          res.send(response)
+        })
+        .catch(err => {
+          console.error('error in server in delete')
+          res.sendStatus(418)
+        })
     })
-
   })
   .catch(err => {
     console.error(err)
