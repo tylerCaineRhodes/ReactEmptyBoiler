ecd94f783012118d74f54d7705e5a2d8ff417534
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-04-27 15:41:20 -0500

    Fix spacings

--- db/querys.js
+++ db/querys.js
@@ -11,10 +11,10 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
   if(err){
-    console.log('couldn\'t connect to database')
+    console.log('couldn\'t connect to database');
   } else {
-    console.log('connected to mysql database!')
-  }
+    console.log('connected to mysql database!');
+  };
 });
 
 const postTask = (task) => {
@@ -22,40 +22,40 @@ const postTask = (task) => {
     const query = 'insert into tasks (task) values (?)';
     connection.query(query,[task],(err, data) => {
       if(err){
-        console.log('something went wrong in posting a task in query')
-        reject(err)
+        console.log('something went wrong in posting a task in query');
+        reject(err);
       } else {
-        resolve(data)
-      }
-    })
-  })
-}
+        resolve(data);
+      };
+    });
+  });
+};
 
 const getTasks = () => {
   return new Promise((resolve, reject) => {
     connection.query(`select * from tasks`,(err, data) => {
       if(err){
-        console.log('something went wrong when getting all tasks in query')
-        reject(err)
+        console.log('something went wrong when getting all tasks in query');
+        reject(err);
       } else {
-        resolve(data)
-      }
-    })
-  })
-}
+        resolve(data);
+      };
+    });
+  });
+};
 
 const deleteTask = (id) => {
   return new Promise((resolve, reject) => {
     const query = 'delete from tasks where id = ?'
     connection.query(query, [id], (err, data) => {
       if(err){
-        console.log('something went wrong with deleting a task in the query')
-        reject(err)
+        console.log('something went wrong with deleting a task in the query');
+        reject(err);
       } else {
-        resolve(data)
-      }
-    })
-  })
-}
+        resolve(data);
+      };
+    });
+  });
+};
 
-module.exports = {postTask, getTasks, deleteTask}
+module.exports = {postTask, getTasks, deleteTask};
